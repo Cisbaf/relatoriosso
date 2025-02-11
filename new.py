a1adf12f435a714d67b3c0dbf5d7f6f915ac24c8
Daniel <danielfernandes202@gmail.com>
2025-02-11 10:44:09 -0300

    rs

--- new.py
+++ new.py
@@ -4,11 +4,13 @@ import os
 from source.infra.sso.controller import SSOController
 from source.domain.request import Request
 from source.domain.date import Date
-from source.infra.relatorios.tempo_resposta import processing, critico
+from source.infra.relatorios.analitico import (
+    critico, destino, diaxhorario, one, processing,regulacao, three, tih, total_envios, two
+)
 
 from source.infra.bigquery.big import BigQueryRepository
 
-time.sleep(30)
+
 load_dotenv(override=True)
 
 url = os.getenv("URL")
@@ -17,19 +19,39 @@ table_id = os.getenv("TABLEID")
 user = os.getenv("USER")
 password = os.getenv("PASSWORD")
 
-coockie = SSOController("Daniel.Fernandes", "42658265", "http://localhost:4444/wd/hub").get_coockie()
+print("pegando coockie 1/4")
+coockie = SSOController(user, password, "http://localhost:4444/wd/hub").get_coockie()
 
 request = Request(
     url="https://cisbaf.ssosamu.com:3001/SSONovaIguacu/_Relatorio/frmConsultaRelatorioNovo.aspx",
     coockie=coockie,
-    date_in=Date.split("01/01/2024"),
-    date_fim=Date.split("30/12/2024")
+    date_in=Date.split("06/02/2025"),
+    date_fim=Date.split("08/02/2025")
 )
 
-analise = processing.TempoRespostaProcessing(relatorios=[
-    critico.RelatorioCritico(name="Critico", request=request),
+print("baixando e tratando dados 2/4 ")
+analise = processing.AnaliticoProcessing(relatorios=[
+    critico.AnaliticoCritico(name="Critico", request=request),
+    regulacao.AnaliticoRegulacao(name="Regulação", request=request),
+    tih.AnaliticoTIH(name="TIH", request=request),
+    one.ChamadosOne(name="Chamado PT 1", request=request, principal=True),
+    two.ChamadosTwo(name="Chamado PT 2", request=request),
+    three.ChamadosThree(name="Chamado PT 3", request=request),
+    destino.DestinoPaciente(name="Destino Paciente", request=request),
+    diaxhorario.DiaXHorario(name="Dia X Horario", request=request),
+    total_envios.TotalEnvios(name="Total ENvios", request=request)
 ])
 
-relatorio = analise.relatorios[0]
+big = BigQueryRepository(
+    project_id=project_id,
+    table_id=table_id
+)
 
-print(len(relatorio.df))
+print("tratando dados big query 3/4")
+df, last = analise.treat_for_bigquery(
+        df=analise.reform(df=analise.processing())
+    )
+
+print("inserindo dados no bigquery 4/4")
+big.insert_data(df, last)
+print("finalizado!")
